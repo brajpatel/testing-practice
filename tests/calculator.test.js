bcf80983e208cc9eabd0a88768f83f6e94584618
brajpatel <brajeshpatel0002@gmail.com>
2023-03-11 13:31:50 +0000

    Add calculator multiply and divide tests

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -4,6 +4,30 @@ test('calculator add test 1', () => {
     expect(calculator.add(20, 40)).toBe(60);
 })
 
-test('calculator add test 1', () => {
+test('calculator add test 2', () => {
     expect(calculator.add(1234, 4321)).toBe(5555);
+})
+
+test('calculator subtract test 1', () => {
+    expect(calculator.subtract(15, 15)).toBe(0);
+})
+
+test('calculator subtract test 2', () => {
+    expect(calculator.subtract(1021, 637)).toBe(384);
+})
+
+test('calculator multiply test 1', () => {
+    expect(calculator.multiply(12, 12)).toBe(144);
+})
+
+test('calculator multiply test 2', () => {
+    expect(calculator.multiply(238, 591)).toBe(140658);
+})
+
+test('calculator divide test 1', () => {
+    expect(calculator.divide(1000, 10)).toBe(100);
+})
+
+test('calculator divide test 2', () => {
+    expect(calculator.divide(23257, 13)).toBe(1789);
 })
